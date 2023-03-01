a6830d8f6033277c4529a31f742be7e2c5d18955
abhishek-goutam <abhishekgoutam8894@gmail.com>
2023-03-01 21:52:17 +0530

    Express task 3

--- npm/index.js
+++ npm/index.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const http = require('http');
-
+const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("In the Middleware");
-    next();
+app.use(bodyParser.urlencoded());
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In the Other Middleware!");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="quantity"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 })
 
-app.use((req,res,next)=>{
-    console.log("In the Other Middleware");
-    res.send('<h1>Hello from Express</h1>')
-})
+app.use("/", (req, res, next) => {
+  console.log("In the Other Middleware");
+  res.send("<h1>Hello from Express</h1>");
+});
 
 app.listen(3000);
-
-
-
