59464d2d872f755ce0c187a740882d028ce6cee4
abhishek-goutam <abhishekgoutam8894@gmail.com>
2023-03-05 21:55:08 +0530

    cleaning up the code

--- npm/index.js
+++ npm/index.js
@@ -1,24 +1,14 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use(bodyParser.urlencoded());
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In the Other Middleware!");
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="quantity"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found!</h1>')
 })
-
-app.use("/", (req, res, next) => {
-  console.log("In the Other Middleware");
-  res.send("<h1>Hello from Express</h1>");
-});
-
 app.listen(3000);
